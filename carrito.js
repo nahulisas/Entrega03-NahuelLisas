b9422de24cc3adfbc9279edddacc4f8f462a5c8d
Nahuel <nahulisas@gmail.com>
2024-08-12 21:37:07 -0300

    se mejoran los estilos

--- carrito.js
+++ carrito.js
@@ -9,23 +9,23 @@ let injectarProductos = (array) => {
     carritoContainer.innerHTML = ''
     array.forEach(elemento => {
         let producto = document.createElement('div')
-        producto.innerHTML = ` 
+        producto.innerHTML = `
             <div class="carrito_item">
                     <div class="item_box">
-                        <img src="" alt="imagen" />
-                        <p>${elemento.nombre}</p>
+                        <img src="${elemento.images}" alt="imagen" />
+                        <p>${elemento.title}</p>
                     </div>
                     <div class="item_box">
                         <div class="modificar_cantidad">
                             <button onclick="restarCantidad(${elemento.id})" id="restar_cantidad">
                                 <i class="bi bi-dash"></i>
                             </button>
-                            <p class="cantidad">${elemento.cantidad}</p>
+                            <span class="cantidad">${elemento.cantidad}</span>
                             <button onclick="sumarCantidad(${elemento.id})" id="sumar_cantidad">
                                 <i class="bi bi-plus"></i>
                             </button>
                         </div>
-                        <p class="total">$${elemento.precio * elemento.cantidad}</p>
+                        <p class="total">$${elemento.price * elemento.cantidad}</p>
                         <button onclick="eliminarDelCarrito(${elemento.id})" class="eliminar_producto">
                             <i class="bi bi-trash"></i>
                         </button>
@@ -43,11 +43,31 @@ injectarProductos(carrito)
 // Eliminar productos del carrito
 
 let eliminarDelCarrito = (id) => {
-    let productoEliminado = carrito.findIndex(producto => producto.id === id)
-    carrito.splice(productoEliminado, 1)
-    localStorage.setItem('carrito', JSON.stringify(carrito))
-    injectarProductos(carrito)
-    contadorCarrito.innerText = carrito.length
+    Swal.fire({
+        title: "Desea eliminar este producto del carrito?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Eliminar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let indiceDelProductoAEliminar = carrito.findIndex(producto => producto.id === id)
+            carrito.splice(indiceDelProductoAEliminar, 1)
+            localStorage.setItem('carrito', JSON.stringify(carrito))
+            injectarProductos(carrito)
+            contadorCarrito.innerText = carrito.length
+            Swal.fire({
+                title: 'Eliminado',
+                icon: "success",
+                toast: true,
+                position: "bottom",
+                timer: 3000,
+                showConfirmButton: false,
+            });
+        }
+    });
 }
 
 sumarCantidad = (id) => {
--- script.js
+++ script.js
@@ -1,105 +1,79 @@
 // Arrays
-let misProductos = JSON.parse(localStorage.getItem('misProductos')) || []
-// let misProductos = [
-//     {
-//         nombre: "Remera",
-//         precio: Number(1500),
-//         descripcion: 'El mejor',
-//         categoria: "remeras",
-//         id: 1,
-//     },
-//     {
-//         nombre: "Campera",
-//         precio: Number(4500),
-//         descripcion: 'La mas abrigada',
-//         categoria: "camperas",
-//         id: 2,
-//     },
-//     {
-//         nombre: "Pantalon",
-//         precio: Number(3799),
-//         descripcion: 'El mas mejor',
-//         categoria: "pantalones",
-//         id: 3,
-//     },
-//     {
-//         nombre: "Zapatilla",
-//         precio: Number(10000),
-//         descripcion: 'La mas comoda',
-//         categoria: "zapatillas",
-//         id: 4,
-//     }
-// ]
+// let misProductos = JSON.parse(localStorage.getItem('misProductos')) || []
 let carrito = JSON.parse(localStorage.getItem('carrito')) || []
 
-// Objetos del DOM
-let formContainer = document.querySelector('.form_container')
-let productContainer = document.querySelector('.product_container')
-let btnCrearProducto = document.getElementById('drop_form')
-let btnAgregarProducto = document.getElementById('agregar_producto')
-let myForm = document.getElementById('form')
-let nombre = document.getElementById('nombre')
-let precio = document.getElementById('precio')
-let descripcion = document.getElementById('descripcion')
-let categoriaProducto = document.getElementById('categoria')
 
+let misProductos = []
+let getProductos = fetch("https://api.escuelajs.co/api/v1/products");
+
+getProductos
+    .then((res) => res.json())
+    .then((res) => {
+        misProductos = res;
+        injectarProductos(misProductos)
+    })
+    .catch((error) => console.log(error))
+
+
+// Objetos del DOM
+
+let productContainer = document.querySelector('.product_container')
+let categoriaProducto = document.getElementById('categoria')
 let contadorCarrito = document.getElementById("carrito_contador")
 contadorCarrito.innerText = carrito.length
 
 
 // Agregar productos a mi Array de productos
 
-let id = 0
 
-let agregar_producto = () => {
-
-    let producto = {
-        nombre: nombre.value,
-        precio: Number(precio.value),
-        descripcion: descripcion.value,
-        categoria: categoriaProducto.value,
-        id: id++,
-    }
-
-
-    if (nombre.value === '' || precio.value === '' || descripcion.value === '' || categoriaProducto.value === '') {
-        alert('todos los campos son obligatorios')
-    } else {
-        let productoRepetido = misProductos.find(elemento => elemento.nombre == producto.nombre)
-        if (productoRepetido) {
-            alert('Ya existe un producto con ese nombre')
-        } else {
-            misProductos.unshift(producto)
-            injectarProductos(misProductos)
-        }
-
-        localStorage.setItem('misProductos', JSON.stringify(misProductos))
-    }
-}
 
 
 // injectar mis productos en el dom
+
 let injectarProductos = (array) => {
     productContainer.innerHTML = ''
+
+
     array.forEach(elemento => {
+
+        let buscarEnElCarrito = carrito.find(item => item.id == elemento.id)
         let producto = document.createElement('div')
-        producto.innerHTML = ` 
-        <div class="product_card">
-        <div class="product_card_img">
-        <i class="bi bi-image"></i>
-        </div>
-        <div class="product_card_info">
-        <div class="product_card_title">${elemento.nombre}</div>
-        <div class="product_card_price">$${elemento.precio}</div>
-        <div class="product_card_description">
-        ${elemento.descripcion}
-        </div>
-        <div class="">
-        <a class="btn-agregar-carro" onclick="agregarAlCarrito(${elemento.id})">Agregar al carrito</a>
-        </div>
-        </div>
-        </div>
+
+        if (buscarEnElCarrito) {
+            producto.innerHTML = ` 
+            <div class="product_card on_cart">
+            <div class="product_card_img ">
+            <img src="${elemento.images[0]}">
+            </div>
+            <div class="product_card_info">
+            <div class="product_card_title">${elemento.title}</div>
+            <div class="product_card_price">$${elemento.price}</div>
+            </div>
+            <div class="agregar-carro-box">
+            <a class="btn-agregar-carro" onclick="agregarAlCarrito(${elemento.id})">En carrito</a>
+            <i class="bi bi-cart-check-fill on_cart_icon"></i>
+            </div>
+            </div>
         `
+        } else {
+            producto.innerHTML = ` 
+            <div class="product_card">
+            <div class="product_card_img">
+            <img src="${elemento.images[0]}">
+            </div>
+            <div class="product_card_info">
+            <div class="product_card_title">${elemento.title}</div>
+            <div class="product_card_price">$${elemento.price}</div>
+            </div>
+            <div class="agregar-carro-box">
+            <a class="btn-agregar-carro" onclick="agregarAlCarrito(${elemento.id})">Agregar al carrito</a>
+            <i class="bi bi-cart-check-fill on_cart_icon"></i>
+            </div>
+            </div>
+        `
+
+        }
+
 
         productContainer.appendChild(producto)
         localStorage.setItem('misProductos', JSON.stringify(misProductos))
@@ -116,41 +90,68 @@ let dropForm = () => {
 
 
 
-// eventos
-btnAgregarProducto.addEventListener('click', agregar_producto)
-btnCrearProducto.addEventListener("click", dropForm)
 
 
-// filtrar productos por su categoria
-let listItem = document.querySelectorAll('.item_menu')
+// filtrar de productos
 
-function filtrarCategorias(categoria) {
-    let productosFiltrados = misProductos.filter(producto => producto.categoria == categoria || 'todos_los_productos' == categoria)
-    injectarProductos(productosFiltrados)
-}
+let inputSearch = document.getElementById('input_search')
 
-listItem.forEach(item => {
-    item.addEventListener('click', event => {
-        let categoria = event.target.id
-        filtrarCategorias(categoria)
-    })
-})
+
+// let listItem = document.querySelectorAll('.item_menu')
+
+// function filtrarCategorias(categoria) {
+//     let productosFiltrados = misProductos.filter(producto => producto.categoria == categoria || 'todos_los_productos' == categoria)
+//     injectarProductos(productosFiltrados)
+// }
+
+// listItem.forEach(item => {
+//     item.addEventListener('click', event => {
+//         let categoria = event.target.id
+//         filtrarCategorias(categoria)
+//     })
+// })
 
 // agregar productos al carrito
 
+
 let agregarAlCarrito = (id) => {
+
+    let addedOnCartIcon = document.querySelector(".product_card")
+    let btnAgregarCarro = document.querySelector('.btn-agregar-carro')
     let agregarProducto = misProductos.find(producto => producto.id === id)
     let buscarEnElCarrito = carrito.find(elemento => elemento.id == agregarProducto.id)
 
     if (buscarEnElCarrito) {
-        buscarEnElCarrito.cantidad++
+        Swal.fire({
+            title: `${agregarProducto.title} ya se encuentra en el carrito`,
+            position: "bottom",
+            timer: 2000,
+            showConfirmButton: false,
+            toast: true,
+        });
+
     } else {
         carrito.push({ ...agregarProducto, cantidad: 1, })
+        addedOnCartIcon.classList.add('on_cart')
+        btnAgregarCarro.textContent = 'En carrito'
         contadorCarrito.innerText = carrito.length
+        injectarProductos(misProductos)
+
+        Swal.fire({
+            title: `${agregarProducto.title} agregado al carrito`,
+            position: "bottom",
+            timer: 1000,
+            showConfirmButton: false,
+            toast: true,
+        });
     }
+
 
     localStorage.setItem('carrito', JSON.stringify(carrito))
 }
+
+
+
 
 
 
